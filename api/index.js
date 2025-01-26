214c9db30c43b81b8602d9a6a9522f0b06c438de
faizan0209 <muhammadfaizansajid0@gmail.com>
2025-01-26 07:52:41 +0500

    update

--- api/index.js
+++ api/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const connectDB = require("../config/db");
+// const connectDB = require("../config/db");
 
 dotenv.config();
-connectDB();
+// connectDB();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/users", require("../routes/authRoutes"));
-
+app.post("/", ()=>{
+    res.send({message:"send"})
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
